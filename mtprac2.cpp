c1cf1969f9b9789275706a7d4d4f1f59a8a01367
Julian Cadenas <jscadenas12@gmail.com>
2024-02-03 13:51:34 +0800

    added counter, reverse and find

--- mtprac2.cpp
+++ mtprac2.cpp
@@ -1,93 +1,132 @@
 #include <iostream>
+#include <string>
 
-//set the linked list up
+
+// INITIALIZE LINKED LIST
 struct Node{
     int data;
     Node* next;
 };
 
-//push means insert at the top/front
-void push(Node**head,int num){
-    Node* newNode=new Node;
-    newNode->data=num;
-    newNode->next=*head;
-    *head=newNode;
-}
 
-//insert means insert at the end
-void insert(Node**node,int num){
-    Node* newNode=new Node;
-    Node* last=*node;
-    newNode->data=num;
-    newNode->next=NULL;
-    if(*node==NULL){
-        *node=newNode;
-        return;
-    }
-    while(last->next!=NULL){
-        last=last->next;
-    }
-    last->next=newNode;
-}
-
-//insert after a given node
-void insertAfter(Node* prev,int num){
-    Node* newNode=new Node;
-    if(prev==NULL){
-        std::cout<<"your list is empty"<<std::endl;
-        return;
-    }
-    newNode->data=num;
-    newNode->next=prev->next;
-    prev->next=newNode;
-}
-
-void del(Node* node,int num){
-    Node* nodePtr, *previousNode;
-    if (node==NULL){
-        return;        
-    }
-    if (node->data==num){
-        nodePtr=node->next;
-        delete node;
-        node=nodePtr;
-    }else{
-        nodePtr=node;
-        while (nodePtr!=NULL && nodePtr->data!=num){
-            previousNode = nodePtr;
-            nodePtr = nodePtr->next;
-        }
-        previousNode->next = nodePtr->next;
-        delete nodePtr;
-    }
-}
-
-void display(Node* node){
+// DISPLAY THE LINKE LIST AND THE NUMBER OF NODES
+void display(Node *node,int counter){
     if(node==NULL){
-        std::cout<<"your list is empty"<<std::endl;
-    }    
-    while(node!=NULL){
-        std::cout<<node->data<<std::endl;
+        std::cout<<"your list is empty";
+        return;
+    }while(node!=NULL){
+        std::cout<<node->data<<" ";
         node=node->next;
     }
+    std::cout<<std::endl;
+    std::cout<<"the total number of nodes are: "<<counter<<std::endl;
 }
 
-int main() {
 
-    Node* head=NULL; //initialize your list;
+// DELETE A SPECIFIC NODE
+void del(Node* node,int num,int &counter){
+    Node* nodeptr,*prev;
+    if(node==NULL){
+        return;
+    }
+    if(node->data==num){
+        nodeptr=node->next;
+        delete node;
+        node=nodeptr;
+        counter--;
+    }else{
+        nodeptr=node;
+        while(nodeptr->next!=NULL && nodeptr->data!=num){
+            prev=nodeptr;
+            nodeptr=nodeptr->next;
+        }
+        prev->next=nodeptr->next;
+        delete nodeptr;
+        counter--;
+    }
+}
+
+
+// INSERT A NODE AT THE FRONT OR TOP WITH COUNTER
+void push(Node**node,int num,int &counter){
+    Node* newnode=new Node;
+    newnode->next=*node;
+    newnode->data=num;
+    *node=newnode;
+    counter++;
+}
+
+
+// INSERT A NODE AT THE END OR BOTTOM WITH COUNTER
+void insert(Node**node,int num,int &counter){
+    Node* newnode=new Node;
+    Node* last=*node;
+    newnode->data=num;
+    newnode->next=NULL;
+    if(*node==NULL){
+        *node=newnode;
+        counter++;
+        return;
+    }while(last->next!=NULL){
+        last=last->next;
+    }
+    counter++;
+    last->next=newnode;
+}
+
+// REVERSE THE LINKED LIST
+void reverse(Node* &node){
+    Node* nodeptr=node;
+    Node* prev=NULL;
+    Node* next=NULL;
+    while(nodeptr!=NULL){
+        next=nodeptr->next;
+        nodeptr->next=prev;
+        prev=nodeptr;
+        nodeptr=next;
+    }
+    node=prev;
+}
+
+
+// FIND A SPECIFIC NODE
+void find(Node* node,int num){
+    bool found=false;
+    while(node!=NULL){
+        if(node->data==num){
+            std::cout<<"node found: "<<node->data<<std::endl;
+            found=true;
+        }
+        node=node->next;
+    }
+    if(found==false){
+        std::cout<<"node not found"<<std::endl;
+    }
+
+}
+
+
+int main(){
     
-    push(&head,69);
-    push(&head->next,420);
-    insert(&head,911);
-    insert(&head,16);
-    insert(&head,1);
-    insert(&head,2);
-    insert(&head,3);
-    insert(&head,4);
-    insertAfter(head->next->next,20); //insert after 911
-    del(head,16);
-    display(head);
-
+    Node* head=NULL;
+    int counter=0;
+    // original(unreveresed) output is 69 420 911 16 69420
+    insert(&head,20,counter);
+    insert(&head,16,counter);
+    push(&head,69,counter);
+    push(&head->next,420,counter);
+    push(&head->next->next,911,counter);
+    insert(&head,69420,counter);
+    del(head,20,counter);
+    reverse(head);
+    display(head,counter);
+    find(head,20);
+    /* OUTPUT SHOULD BE:  
+        69420 16 911 420 69 
+        the total number of nodes are: 5
+        node not found            
+    */
 
     return 0;
 }
+
